2ce654651cb7475dc28c745d7a081a58176b94fc
hui.yee.leong <hui.yee.leong@accenture.com>
2022-12-06 22:02:09 +1100

    Updated New AWS EKS CICD CodeBuild

---
+++ test/aws-eks-cicd-codebuild.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsEksCicdCodebuild from '../lib/aws-eks-cicd-codebuild-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-eks-cicd-codebuild-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsEksCicdCodebuild.AwsEksCicdCodebuildStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
